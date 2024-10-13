70058eea9fc2281c4250eddc967973bac5a4b53d
Valery Vishnevezky <valery020201@gmail.com>
2024-10-13 16:04:07 +0300

    Add options for rating in a reviews

---
+++ cmps/RateBySelect.jsx
@@ -0,0 +1,11 @@
+export function RateBySelect({ val, onSelected }) {
+    return (
+      <select className="rate-by-select" value={val} onChange={(e) => onSelected(e.target.value)}>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+    )
+  }
---
+++ cmps/RateByTextbox.jsx
@@ -0,0 +1,13 @@
+export function RateByTextbox({ val, onSelected }) {
+    return (
+      <div className="rate-by-textbox">
+        <input
+          type="number"
+          value={val}
+          min="1"
+          max="5"
+          onChange={(e) => onSelected(e.target.value)}
+        />
+      </div>
+    )
+  }
